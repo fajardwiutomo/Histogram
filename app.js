7d291bf887fceb64ab6248f28fa83d3d1c2a1700
Jonathan Sebastian <j.shn50@gmail.com>
2022-02-24 16:27:03 +0700

    bcrypt session middleware

--- app.js
+++ app.js
@@ -2,11 +2,36 @@ const express = require('express')
 const app = express()
 const port = 3000
 const Controller = require('./controllers/controller')
+const UserController = require('./controllers/UserController')
+const session = require('express-session')
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({extended:false}))
+app.use(session({
+  secret: 'rahasia perusahaan',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {
+    secure: false,
+    sameSite: true
+  }
+}))
 
 app.get('/', Controller.landingPage)
+app.get('/register', UserController.registForm)
+app.post('/register', UserController.postRegist)
+app.get('/login', UserController.loginForm)
+app.post('/login', UserController.postLogin)
+app.use(function (req, res, next) {
+  // console.log(req.session)
+  if (!req.session.userId) {
+    const error = 'Please login first!'
+    res.redirect(`/login?error=${error}`)
+  } else {
+    next()
+  }
+})
+app.get('/:id/delete', UserController.deleteUser)
 app.get('/post', Controller.post)
 app.get('/registration', Controller.register)
 
---
+++ controllers/UserController.js
@@ -0,0 +1,67 @@
+const { User } = require('../models')
+const bcrypt = require('bcryptjs')
+class UserController {
+  static registForm(req, res) {
+    res.render('registform')
+  }
+  static postRegist(req, res) {
+    const { userName, email, password, role } = req.body;
+    // console.log(req.body);
+    User.create({ userName, email, password, role })
+      .then(_ => {
+        res.redirect('/login')
+      })
+      .catch(err => {
+        res.send(err);
+      })
+  }
+  static loginForm(req, res) {
+    const { error } = req.query
+    res.render('loginform', { error })
+  }
+  static postLogin(req, res) {
+    const { email, password } = req.body
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          const isValidPassword = bcrypt.compareSync(password, user.password)
+          if (isValidPassword) {
+            req.session.userId = user.id
+            return res.redirect('/')
+          } else {
+            const error = 'invalid email/password'
+            return res.redirect(`/login?error=${error}`)
+          }
+        } else {
+          const error = 'invalid email/password'
+          return res.redirect(`/login?error=${error}`)
+        }
+      })
+      .catch(err => {
+        res.send(err);
+      })
+  }
+  static logOut(req, res) {
+    req.session.destroy((err) => {
+      if (err){
+        res.send(err)
+      } else {
+        res.redirect('login')
+      }
+    })
+  }
+  static deleteUser(req, res) {
+    const { id } = req.params
+    User.destroy({
+      where: { id: +id }
+    })
+      .then(_ => {
+        res.redirect('/login')
+      })
+      .catch(err => {
+        res.send(err)
+      })
+  }
+}
+
+module.exports = UserController;
